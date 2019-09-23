d739b447daf749b19852ad2d2f5f0a2c373202f5
Ildar107 <ildar107wk@gmail.com>
2019-09-23 14:16:09 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -38,7 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    var result = "";
+    var temp = "";
+    var spaceWord = "";
+    for(var i=0; i < expr.length; i+=10)
+    {
+        var subStr = expr.substring(i,i + 10).replace(/00/g, "").replace(/10/g, ".").replace(/11/g, "-");
+        if(subStr.includes("*"))
+            result += " ";
+        else 
+            result += MORSE_TABLE[subStr];
+    }
+    if(expr.length % 10 > 0)
+        result += MORSE_TABLE[expr.substring(expr.length - 1 - expr.length % 10, expr.length - 1)
+                                  .replace(/00/g, "")
+                                  .replace(/10/g, ".")
+                                  .replace(/11/g, "-")
+                                ];
+    return result;
 }
 
 module.exports = {
